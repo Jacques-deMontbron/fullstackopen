2449d019360d826af34176bb6240fce16cb522be
Jacques de Montbron <Jacques-deMontbron@users.noreply.github.com>
2022-06-13 14:16:01 +0200

    unicafe step2 Finished

--- part1/unicafe/src/components/Statistics.js
+++ part1/unicafe/src/components/Statistics.js
@@ -1,10 +1,18 @@
 const Statistics = ({ good, neutral, bad }) => {
+
+    const nbAllFeedbacks = good + neutral + bad
+    const average = nbAllFeedbacks == 0 ? 0 : ((good - bad) / nbAllFeedbacks)
+    const positiveRatio = nbAllFeedbacks == 0 ? 0 : (good / nbAllFeedbacks)
+
     return (
         <>
             <h1>statistics</h1>
             <p>good {good}</p>
             <p>neutral {neutral}</p>
             <p>bad {bad}</p>
+            <p>all {nbAllFeedbacks}</p>
+            <p>average {average}</p>
+            <p>positive {positiveRatio * 100}%</p>
         </>
     )
 }
