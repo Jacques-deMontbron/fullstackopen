6b09b2f720d7b2b0cd965f421cebde06ac903444
Jacques de Montbron <Jacques-deMontbron@users.noreply.github.com>
2022-06-13 14:40:39 +0200

    unicafe step5 finished

--- part1/unicafe/src/components/Statistics.js
+++ part1/unicafe/src/components/Statistics.js
@@ -1,3 +1,5 @@
+import StatisticLine from "./StatisticLine"
+
 const Statistics = ({ good, neutral, bad }) => {
 
     const nbAllFeedbacks = good + neutral + bad
@@ -15,12 +17,12 @@ const Statistics = ({ good, neutral, bad }) => {
         return (
             <>
                 <h1>statistics</h1>
-                <p>good {good}</p>
-                <p>neutral {neutral}</p>
-                <p>bad {bad}</p>
-                <p>all {nbAllFeedbacks}</p>
-                <p>average {average}</p>
-                <p>positive {positiveRatio * 100}%</p>
+                <StatisticLine text="good" value={good} />
+                <StatisticLine text="neutral" value={neutral} />
+                <StatisticLine text="bad" value={bad} />
+                <StatisticLine text="all" value={nbAllFeedbacks} />
+                <StatisticLine text="average" value={average} />
+                <StatisticLine text="positive" value={positiveRatio * 100 + "%"} />
             </>
         )
     }
