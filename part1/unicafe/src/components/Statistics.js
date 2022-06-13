4fa8a40da4894e26643e69e4bd25153053f74003
Jacques de Montbron <Jacques-deMontbron@users.noreply.github.com>
2022-06-13 14:22:00 +0200

    unicafe step3 & step4 Finished

--- part1/unicafe/src/components/Statistics.js
+++ part1/unicafe/src/components/Statistics.js
@@ -1,20 +1,29 @@
 const Statistics = ({ good, neutral, bad }) => {
 
     const nbAllFeedbacks = good + neutral + bad
-    const average = nbAllFeedbacks == 0 ? 0 : ((good - bad) / nbAllFeedbacks)
-    const positiveRatio = nbAllFeedbacks == 0 ? 0 : (good / nbAllFeedbacks)
-
-    return (
-        <>
-            <h1>statistics</h1>
-            <p>good {good}</p>
-            <p>neutral {neutral}</p>
-            <p>bad {bad}</p>
-            <p>all {nbAllFeedbacks}</p>
-            <p>average {average}</p>
-            <p>positive {positiveRatio * 100}%</p>
-        </>
-    )
+    const average = nbAllFeedbacks === 0 ? 0 : ((good - bad) / nbAllFeedbacks)
+    const positiveRatio = nbAllFeedbacks === 0 ? 0 : (good / nbAllFeedbacks)
+    if (nbAllFeedbacks === 0) {
+        return (
+            <>
+                <h1>statistics</h1>
+                <p> No feedback given</p >
+            </>
+        )
+    }
+    else {
+        return (
+            <>
+                <h1>statistics</h1>
+                <p>good {good}</p>
+                <p>neutral {neutral}</p>
+                <p>bad {bad}</p>
+                <p>all {nbAllFeedbacks}</p>
+                <p>average {average}</p>
+                <p>positive {positiveRatio * 100}%</p>
+            </>
+        )
+    }
 }
 
 export default Statistics
